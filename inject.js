cc824eec80d018b58609632ecefba70d1937451e
prathul sai <32157444+prathulsai@users.noreply.github.com>
2020-03-31 18:11:05 +0530

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,18 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+
+  document.body.style.border = "5px solid red";
+
+  document.getElementsByTagName("button")[1].click();
+  
+  document.getElementsByTagName("label")[3].click();
+  
+  document.getElementsByTagName("li")[5].click();
+  
+  document.getElementsByTagName("li")[11].click();
+  
+  document.getElementsByTagName("button")[3].click();
+  
+})();
